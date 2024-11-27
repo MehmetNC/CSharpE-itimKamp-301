e03bdd67e7914c88a0f1d36548639f5c02218020
Mehmet Nur CEYLAN <ceylanmehmetnur92@gmail.com>
2024-11-27 16:17:56 +0300

    Add project files.

---
+++ CSharpEğitimKampı301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEğitimKampı301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
+
+/*
+ Field-Veriable-Property
+ */
+/*
+ int x; şeklinde sınıfın içine tanımlanırsa Field olur.
+public int y {get;set;} olursa property olur.
+void test()
+        {
+            int z; şeklinde metod olarak tanımlanırsa veriable olur.
+        }
+ */
