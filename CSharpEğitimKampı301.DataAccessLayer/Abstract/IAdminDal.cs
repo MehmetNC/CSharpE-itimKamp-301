b9fdc3f5f2022b4e70bd2ab284ddd6d25c79482c
Mehmet Nur CEYLAN <ceylanmehmetnur92@gmail.com>
2024-11-28 11:23:49 +0300

    Migration İşlemleri ve Abstract Interfaceler

---
+++ CSharpEğitimKampı301.DataAccessLayer/Abstract/IAdminDal.cs
@@ -0,0 +1,13 @@
+﻿using CSharpEğitimKampı301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEğitimKampı301.DataAccessLayer.Abstract
+{
+    public interface IAdminDal:IGenericDal<Admin>
+    {
+    }
+}
