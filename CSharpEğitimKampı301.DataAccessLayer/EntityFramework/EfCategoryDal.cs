a4890da9d8ab7ad59bee6e655eba51df045770a4
Mehmet Nur CEYLAN <ceylanmehmetnur92@gmail.com>
2024-12-16 16:01:12 +0300

    EntityState Komutları, Generic Repository Sınıfı ve Ef Sınıfları

---
+++ CSharpEğitimKampı301.DataAccessLayer/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,15 @@
+﻿using CSharpEğitimKampı301.DataAccessLayer.Abstract;
+using CSharpEğitimKampı301.DataAccessLayer.Repositories;
+using CSharpEğitimKampı301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEğitimKampı301.DataAccessLayer.EntityFramework
+{
+    public class EfCategoryDal:GenericRepository<Category>,ICategoryDal
+    {
+    }
+}
