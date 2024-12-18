01aae9009472780f15afe90b89f65fa2925cbd3c
Mehmet Nur CEYLAN <ceylanmehmetnur92@gmail.com>
2024-12-18 18:25:34 +0300

    Business Katmanı ve Logic Kurallar

---
+++ CSharpEğitimKampı301.BusinessLayer/Abstract/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using CSharpEğitimKampı301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEğitimKampı301.BusinessLayer.Abstract
+{
+    public interface IOrderService:IGenericService<Order>
+    {
+    }
+}
