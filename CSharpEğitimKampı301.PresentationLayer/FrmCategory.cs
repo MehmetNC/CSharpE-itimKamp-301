1f90ce98fa48181fdfb02a74baaadc04c5926a50
Mehmet Nur CEYLAN <ceylanmehmetnur92@gmail.com>
2024-12-19 18:50:10 +0300

    Dependency Injection

--- CSharpEğitimKampı301.PresentationLayer/FrmCategory.cs
+++ CSharpEğitimKampı301.PresentationLayer/FrmCategory.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CSharpEğitimKampı301.BusinessLayer.Abstract;
+using CSharpEğitimKampı301.BusinessLayer.Concrete;
+using CSharpEğitimKampı301.DataAccessLayer.Abstract;
+using CSharpEğitimKampı301.DataAccessLayer.EntityFramework;
+using CSharpEğitimKampı301.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +17,51 @@ namespace CSharpEğitimKampı301.PresentationLayer
 {
     public partial class FrmCategory : Form
     {
+        private readonly ICategoryService _categoryService;
+
         public FrmCategory()
         {
+            _categoryService =new CategoryManager(new EfCategoryDal());
             InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var categoryValues = _categoryService.TGetAll();
+            dataGridView1.DataSource = categoryValues;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            category.CategoryName=txtCategoryName.Text;
+            category.CategoryStatus = true;
+            _categoryService.TInsert(category);
+            MessageBox.Show("Ekleme başarılı");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtCategoryId.Text);
+            var deleteValues = _categoryService.TGetById(id);
+            _categoryService.TDelete(deleteValues);
+            MessageBox.Show("Silme başarılı");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtCategoryId.Text);
+            var values=_categoryService.TGetById(id);
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int updatedId=int.Parse(txtCategoryId.Text);
+            var updatedValue = _categoryService.TGetById(updatedId);
+            updatedValue.CategoryName= txtCategoryName.Text;
+            updatedValue.CategoryStatus= true;
+            _categoryService.TUpdate(updatedValue);
         }
     }
 }
